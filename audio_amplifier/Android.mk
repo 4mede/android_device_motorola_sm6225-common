d8265d431bcd62d62b4fcd9b5d0049f1bdb9e2c4
4mede <4mede@tutamail.com>
2024-12-16 09:29:19 -0500

    comment out lines

--- audio_amplifier/Android.mk
+++ audio_amplifier/Android.mk
@@ -7,27 +7,27 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio_amplifier.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := audio_amplifier.c
-LOCAL_VENDOR_MODULE := true
+#LOCAL_MODULE := audio_amplifier.$(TARGET_BOARD_PLATFORM)
+#LOCAL_MODULE_RELATIVE_PATH := hw
+#LOCAL_SRC_FILES := audio_amplifier.c
+#LOCAL_VENDOR_MODULE := true
 
-LOCAL_C_INCLUDES += \
-    $(call include-path-for, audio-route) \
-    $(call include-path-for, audio-utils) \
-    $(call project-path-for, qcom-audio)/hal \
-    $(call project-path-for, qcom-audio)/hal/audio_extn \
-    $(call project-path-for, qcom-audio)/hal/msm8974 \
-    external/tinycompress/include
+#LOCAL_C_INCLUDES += \
+    #$(call include-path-for, audio-route) \
+    #$(call include-path-for, audio-utils) \
+    #$(call project-path-for, qcom-audio)/hal \
+    #$(call project-path-for, qcom-audio)/hal/audio_extn \
+    #$(call project-path-for, qcom-audio)/hal/msm8974 \
+    #external/tinycompress/include
 
-LOCAL_HEADER_LIBRARIES += \
-    generated_kernel_headers \
-    libhardware_headers
+#LOCAL_HEADER_LIBRARIES += \
+    #generated_kernel_headers \
+    #libhardware_headers
 
-LOCAL_SHARED_LIBRARIES += \
-    audio.primary.$(TARGET_BOARD_PLATFORM) \
-    libcutils \
-    liblog \
-    libtinyalsa
+#LOCAL_SHARED_LIBRARIES += \
+    #audio.primary.$(TARGET_BOARD_PLATFORM) \
+    #libcutils \
+    #liblog \
+    #libtinyalsa
 
-include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_SHARED_LIBRARY)
