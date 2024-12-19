fc80f63cca3bd2151457a1aa26031dd8970ae4be
4mede <4mede@tutamail.com>
2024-12-18 21:48:40 -0500

    disable bootjars

--- bengal.mk
+++ bengal.mk
@@ -303,19 +303,19 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
 # Telephony
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-hidl-wrapper-prd \
-    qti_telephony_hidl_wrapper_prd.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
+#PRODUCT_PACKAGES += \
+    #ims-ext-common \
+    #ims_ext_common.xml \
+    #qti-telephony-hidl-wrapper \
+    #qti_telephony_hidl_wrapper.xml \
+    #qti-telephony-hidl-wrapper-prd \
+    #qti_telephony_hidl_wrapper_prd.xml \
+    #qti-telephony-utils \
+    #qti_telephony_utils.xml \
+    #telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+    #telephony-ext
 
 # Thermal
 PRODUCT_PACKAGES += \
