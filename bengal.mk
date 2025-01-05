b7c68050dabe0330870cd18cff62b89735007f6f
4mede <4mede@tutamail.com>
2025-01-05 13:50:24 -0500

    sm6225-common: Flare bringup

--- bengal.mk
+++ bengal.mk
@@ -235,8 +235,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
+    $(LOCAL_PATH)/overlay-flare
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Partitions
